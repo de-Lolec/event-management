1789c18adc833483de778979e0696c4af14b5f93
de-lolec <m_egrishin@webjox.ru>
2023-12-15 04:02:06 +0300

    feat: added migrations, factory and change models

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,10 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'login' => fake()->username(),
+            'name' => fake()->firstName(),
+            'surname' => fake()->lastName(),
+            'date_birth' => fake()->date(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
