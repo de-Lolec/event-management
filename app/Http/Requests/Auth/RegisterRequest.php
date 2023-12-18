02af95df3c6770e89912be14586109d1219a3fa2
de-lolec <m_egrishin@webjox.ru>
2023-12-18 09:04:50 +0300

    dev: deleted unused components

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'surname' => ['required', 'string', 'max:255'],
-            'login' => ['required', 'string', 'max:255'],
+            'login' => ['required', 'string', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ];
     }
