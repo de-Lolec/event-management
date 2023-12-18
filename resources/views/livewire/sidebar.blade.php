f5937e24fd148764114be207a441426e12902460
de-lolec <m_egrishin@webjox.ru>
2023-12-18 07:11:58 +0300

    feat: added logout button

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -1,11 +1,17 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4" wire:poll.30s="updateSidebar">
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="info">
-                <a href="{{ route('admin') }}" class="d-block">
+            <div class="info d-flex justify-content-end">
+                <a href="{{ route('admin') }}" class="d-block mt-2">
                     {{ auth()->user()->name }}
                     {{ auth()->user()->surname }}
                 </a>
+            
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+            
+                    <button type="submit" class="btn btn-dark float-right ml-4">Logout</button>
+                </form>
             </div>
         </div>
         <nav class="mt-2">
